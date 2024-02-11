32ed3b6e2aa1f52903bd4b77f6ce6c062c9bc409
Daniel <daniel@gmail.com>
2024-02-11 23:29:26 +0300

    FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -4,10 +4,27 @@ import mime from 'mime-types';
 import { ObjectID } from 'mongodb';
 import Queue from 'bull';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 const fileQueue = new Queue('fileQueue', 'redis://127.0.0.1:6379');
 
 class FilesController {
+  static async getUser(request) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      const user = await users.findOne({ _id: idObject });
+      if (!user) {
+        return null;
+      }
+      return user;
+    }
+    return null;
+  }
+
   static async postUpload(request, response) {
     const user = await FilesController.getUser(request);
     if (!user) {
